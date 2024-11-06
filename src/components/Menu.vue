511a40f56e8f4e4172819a7661304480d8a77ead
Oh Seung Hyeok <owin1022@naver.com>
2024-11-06 15:21:11 +0900

    feat: 지도 및 메인 버튼

---
+++ src/components/Menu.vue
@@ -0,0 +1,35 @@
+<template lang="">
+  <div class="menu">
+    <div class="btn-group" role="group" aria-label="Basic example">
+      <button type="button" class="btn btn-custom">출석 체크</button>
+      <button type="button" class="btn btn-custom">룰렛</button>
+      <button type="button" class="btn btn-custom">퀴즈</button>
+      <button type="button" class="btn btn-custom" @click="zzanda">짠다</button>
+    </div>
+  </div>
+</template>
+<script setup>
+import { useRouter } from 'vue-router';
+const router = useRouter();
+
+const zzanda = () => {
+  router.push({ name: 'zzanda' });
+};
+</script>
+<style>
+.menu {
+  width: 618px;
+  height: 85px;
+  background-color: white;
+  border-bottom-left-radius: 10px;
+  border-bottom-right-radius: 10px;
+}
+.btn-custom {
+  width: 151.5px;
+  height: 85px;
+  border: 3px solid #ffaf00;
+  border-radius: 10px;
+  font-size: 30px;
+  margin-right: 5px;
+}
+</style>

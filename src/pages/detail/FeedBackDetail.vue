7c1a704bb0d7f879bd7e2d28ac712f20a219b0e7
chanil <hci970705@gmail.com>
2024-11-21 16:17:05 +0900

    fix: FeedBackDetail date 조회시 조회 날짜가 나오지 않는 버그 해결

--- src/pages/detail/FeedBackDetail.vue
+++ src/pages/detail/FeedBackDetail.vue
@@ -89,7 +89,12 @@ const fetchTransactions = async (selectedDate) => {
         const response = await axios.get(
             `/api/boards/feedback/${boardId}/${selectedDate}`
         );
-        console.log(response.data);
+
+        // selectedDate(yyyymmdd)에서 월과 일만 추출하여 포맷팅
+        const month = selectedDate.slice(4, 6);
+        const day = selectedDate.slice(6, 8);
+        formattedDate.value = `${month}/${day}`;
+
         incomeDay.value = response.data.incomeDateSum;
         outcomeDay.value = response.data.outcomeDateSum;
 
